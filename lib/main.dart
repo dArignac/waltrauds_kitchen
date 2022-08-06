18f8b45934669d8fbcc2459a8c98b79bc567aab5
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-08-06 15:18:26 +0200

    some rebranding to Waltraud

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,22 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const WaltraudKitchenApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WaltraudKitchenApp extends StatelessWidget {
+  const WaltraudKitchenApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const name = 'Waltraud\'s Kitchen';
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: name,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: name),
     );
   }
 }
